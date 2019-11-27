dc994d802367458d9fb3ae3ead70736437db9563
Danabek Duisekov <danabekd@gmail.com>
2019-11-27 19:57:44 +0600

    users and roles models

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        DB::table('users')->insert([
+            [
+                'name' => 'Модератор Модератор',
+                'email' => 'moderator@ex.com',
+                'password' => Hash::make('moderator'),
+                'role_id' => 1,
+                'api_token' => Str::random(80),
+            ],
+            [
+                'name' => 'Клиент Клиент',
+                'email' => 'client@ex.com',
+                'password' => Hash::make('client'),
+                'role_id' => 2,
+                'api_token' => Str::random(80),
+            ],
+        ]);
+    }
+}
